6ae159ebfffdfb8ce509c6253020104558dbc92e
StarryNight <tillendaniel@gmail.com>
2026-06-07 13:45:45 +0100

    added build directory for SDL on CMake

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,36 @@
 
 #include <iostream>
-#include <SDL/SDL2.H>
+#include <SDL.h>
 #include <cstdlib>
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello, world!" << std::endl;
-	return EXIT_SUCCESS;
+
+	 if(SDL_Init(SDL_INIT_VIDEO) != 0){
+		std::cout << "SDL_Init Error: " << SDL_GetError();
+		return 1;
+	}
+        
+	SDL_Window* window = SDL_CreateWindow("Caboodle", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 640,480, 0);
+		if(window == NULL){
+		std::cout << "SDL_CreateWindow Error" << SDL_GetError();
+		return 1;
+	}
+
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+	if(renderer == NULL){
+		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError();
+		return 1;
+	}
+
+	SDL_SetRenderDrawColor(renderer , 255 ,255, 255, 255);
+	SDL_RenderClear(renderer);
+	SDL_RenderPresent(renderer);
+
+	SDL_Delay(2000);
+
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+	
+	
 }
